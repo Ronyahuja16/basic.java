73255910108fa5df2bb62f5a0761a72d5bc6bd3e
Gulshan kumar <example@gmail.com>
2024-05-12 22:59:49 +0530

    first commit

---
+++ object.java
@@ -0,0 +1,13 @@
+public class object {
+     static void welcome()
+    {
+        System.out.println("welcome to jehanabad");
+    }
+    public static void main(String args[]){
+        object b1 = new object();
+        b1.welcome();
+
+    }
+
+    
+}
